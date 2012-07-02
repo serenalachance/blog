14ec5e3907e4b216b1693128494d0dd65a706a2e
Serena LaChance <serenalachance@gmail.com>
2012-07-01 19:56:51 -0700

    Adding article "Hypersensitivity"

---
+++ articles/hypersensitivity.rb
@@ -0,0 +1,7 @@
+{ title:         "Hypersensitivity of Sex",
+  slug:          "hypersensitivity",
+  published_at:  Time.parse("Sun Jul  1 19:24:40 PDT 2012"),
+  summary:       "An observation I've made since starting to take this hobby a little more seriously is that even as I try to make myself more convincing, I'm at the same time less convinced by the efforts of others.",
+  location:      "sipping on a rum and coke",
+  featured_html: nil,
+}
